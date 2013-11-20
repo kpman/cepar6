88e6059b2f34006fadf0ee9acec47925731fee02
kpman <s92f002@hotmail.com>
2013-11-21 02:34:25 +0800

    merge ceparSix code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+function resizePages() {
+    var h = $(window).height();
+    var height = h < 1440 ? 1440 : h;
+    $('section').css('height',height);
+    $('.dowhat').css('height',height*2);
+    $('.sponsor').css('height',height*1.6);
+    $('.staff').removeAttr('style');
+}
+resizePages();
+var sk = skrollr.init({forceHeight:false});
 
+$(function(){
+    $(window).resize(function(e) {
+        resizePages();
+        sk.refresh();
+    });
+});
