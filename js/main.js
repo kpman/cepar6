7354560b5bc377e00036493413c059027891c766
kpman <s92f002@hotmail.com>
2013-11-23 20:18:47 +0800

    delete main.css

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 function resizePages() {
     var h = $(window).height();
+    var w = $(window).width();
     var height = h < 1440 ? 1440 : h;
+    var mainPadding = "+=" + (w-1440)/2;
     $('section').css('height',height);
     $('.dowhat').css('height',height*2);
     $('.sponsor').css('height',height*1.6);
     $('.staff').removeAttr('style');
+    $(".main-wrap").css("width",height);
+    $(".main-wrap").css("padding-left",mainPadding);
+
 }
 resizePages();
 var sk = skrollr.init({forceHeight:false});
